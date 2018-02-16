a18aee798d1c7bcf0e279f81f9918d0b18c1e902
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2018-02-16 10:07:04 -0500

    fizzbuzz algorithm solution created in C#

---
+++ fizzbuzz_csharp/fizzbuzz_csharp/fizzbuzz_csharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            fizzBuzz fizzle = new fizzBuzz();
+            fizzle.FizzBuzz(20);
+        }
+    }
+}
---
+++ fizzbuzz_csharp/fizzbuzz_csharp/fizzbuzz_csharp/fizzbuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fizzbuzz_csharp
+{
+    class fizzBuzz
+    {
+        public void FizzBuzz(int inputNum)
+        {
+            for (var i = 1; i <= inputNum; i++)
+            {
+                if (i % 15 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else Console.WriteLine(i);
+            }
+        }
+    }
+}
