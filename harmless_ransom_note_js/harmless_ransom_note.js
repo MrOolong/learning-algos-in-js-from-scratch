101a876bbea1907fcc7c463200778f8b0c17b2cd
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2018-02-19 15:17:58 -0500

    completed algorithm, added notes for time complexity

--- harmless_ransom_note_js/harmless_ransom_note.js
+++ harmless_ransom_note_js/harmless_ransom_note.js
@@ -4,16 +4,19 @@ function harmlessRansomNote(noteText, MagazineText) {
     var magazineObj = {};
 
     // loop through the magazine object to see if the words exist. If words exist, count them
+    // linear time complexity O(n) + O(m) or O(n + m)
+    // O(m)
     magazineArr.forEach(word => {
         if (!magazineObj[word]) magazineObj[word] = 0;
         magazineObj[word]++;
     });
 
     var noteIsPossible = true;
+    // O(n)
     noteArr.forEach(word => {
         if (magazineObj[word]) {
             magazineObj[word]--;
-            if (magazineObj[wprd] < 0) noteIsPossible = false;
+            if (magazineObj[word] < 0) noteIsPossible = false;
         }
         else noteIsPossible = false;
     });
@@ -22,4 +25,5 @@ function harmlessRansomNote(noteText, MagazineText) {
     //console.log(magazineObj);
 }
 
-harmlessRansomNote('', 'this is all the magazine text in the magazine');
+harmlessRansomNote('this is a secret note for you from a secret admirer',
+ 'puerto rico is a place of great wonder and excitement it has many secret waterfall locations that I am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip I have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places I have ever visited');
