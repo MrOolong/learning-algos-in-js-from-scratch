190dee66b72615ce22b29ff33d782bc89014bb5a
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2018-02-19 15:06:12 -0500

    pre-testing

--- harmless_ransom_note_js/harmless_ransom_note.js
+++ harmless_ransom_note_js/harmless_ransom_note.js
@@ -3,5 +3,23 @@ function harmlessRansomNote(noteText, MagazineText) {
     var magazineArr = MagazineText.split(' ');
     var magazineObj = {};
 
-    
+    // loop through the magazine object to see if the words exist. If words exist, count them
+    magazineArr.forEach(word => {
+        if (!magazineObj[word]) magazineObj[word] = 0;
+        magazineObj[word]++;
+    });
+
+    var noteIsPossible = true;
+    noteArr.forEach(word => {
+        if (magazineObj[word]) {
+            magazineObj[word]--;
+            if (magazineObj[wprd] < 0) noteIsPossible = false;
+        }
+        else noteIsPossible = false;
+    });
+
+    return noteIsPossible;
+    //console.log(magazineObj);
 }
+
+harmlessRansomNote('', 'this is all the magazine text in the magazine');
