0371fd266c33576689b8cd6ef381874b93ae3558
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2018-02-19 20:11:52 -0500

    working through converting to C#. stuck on object

--- harmless_ransom_note_csharp/harmless_ransom_note_csharp/harmless_ransom_note_csharp/Program.cs
+++ harmless_ransom_note_csharp/harmless_ransom_note_csharp/harmless_ransom_note_csharp/Program.cs
@@ -10,7 +10,8 @@ namespace harmless_ransom_note_csharp
     {
         static void Main(string[] args)
         {
-            // fillme
+            RansomNote ransomNote1 = new RansomNote();
+            ransomNote1.MakeRansomNote("this is a secret note for you from a secret admirer", "puerto rico is a place of great wonder and excitement it has many secret waterfall locations that I am an admirer of you must hike quite a distance to find the secret places as they are far from populated areas but it is worth the effort a tip I have for you is to go early in the morning when it is not so hot out also note that you must wear hiking boots this is one of the best places I have ever visited");
         }
     }
 }
---
+++ harmless_ransom_note_csharp/harmless_ransom_note_csharp/harmless_ransom_note_csharp/RansomNote.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace harmless_ransom_note_csharp
+{
+    class RansomNote
+    {
+        public Boolean MakeRansomNote(string NoteText, string MagazineText)
+        {
+            // abc
+            string[] noteArr = NoteText.Split(' ');
+            string[] magazineArr = MagazineText.Split(' ');
+            //need to make magazine object
+            var magazineObj = "";
+
+            foreach (string word in magazineArr)
+            {
+                if (!magazineObj[word]) magazineObj[word] = 0;
+                magazineObj[word]++;
+            }
+
+            bool noteIsPossible = true;
+            foreach (var word in noteArr)
+            {
+                if (magazineObj[word])
+                {
+                    magazineObj[word]--;
+                    if (magazineObj[word] < 0) noteIsPossible = false;
+                }
+                else noteIsPossible = false;
+            }
+            return noteIsPossible
+        }
+    }
+}
