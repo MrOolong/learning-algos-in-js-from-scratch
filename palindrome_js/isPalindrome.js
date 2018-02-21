f08bb7249b739292d1e4c27d48955d610fe24f23
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2018-02-20 20:00:22 -0500

    completed isPalindrome algorithm

--- palindrome_js/isPalindrome.js
+++ palindrome_js/isPalindrome.js
@@ -1,5 +1,19 @@
 function isPalindrome(string) {
+    // make all input lowercase using toLowerCase method
+    string = string.toLowerCase();
+    // split string into character array
+    var charactersArr = string.split('');
+    // define valid characters to use against the input
+    var validCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+    
+    // create array
+    var lettersArr = [];
+    charactersArr.forEach(char => {
+        if (validCharacters.indexOf(char) > -1) lettersArr.push(char);
+    });
 
+    if (lettersArr.join('') === lettersArr.reverse().join('')) return true;
+    else return false;
 }
 
-isPalindrome("Madam I'm Adam");
+console.log(isPalindrome("Madam I'm Adam"));
