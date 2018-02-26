685df11de71945948dcdb2068e57020b56ba0ca8
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2018-02-25 23:03:23 -0500

    palindrome algo in cs in progress

---
+++ palindrome_csharp/Palindrome/Palindrome/PalindromeClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class PalindromeClass
+    {
+        public void IsPalindrome(string inputText)
+        {
+            string input = inputText.ToLower();
+            var characterArr = input.Split();
+            var validCharacters = "abcdefghijklmnopqrstuvwxyz".Split();
+
+            char[] lettersArr;
+            foreach (char char1 in characterArr)
+            {
+                if (validCharacters.IndexOf(char1) > -1)
+                
+                    lettersArr.Push(char1);
+                
+            }
+        }
+    }
+}
--- palindrome_csharp/Palindrome/Palindrome/Program.cs
+++ palindrome_csharp/Palindrome/Palindrome/Program.cs
@@ -10,7 +10,7 @@ namespace Palindrome
     {
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
