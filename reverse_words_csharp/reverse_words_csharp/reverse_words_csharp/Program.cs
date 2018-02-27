00acb84d7452346db38e9efd96288df11ef0dc2b
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2018-02-26 20:49:08 -0500

    completed solution to reverse words in csharp

---
+++ reverse_words_csharp/reverse_words_csharp/reverse_words_csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse_words_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReverseWords rv1 = new ReverseWords();
+            var output = rv1.reverseWords("this is a string of words");
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ reverse_words_csharp/reverse_words_csharp/reverse_words_csharp/ReverseWords.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reverse_words_csharp
+{
+    class ReverseWords
+    {
+        public string reverseWords(string input)
+        {
+            string[] wordsArr = input.Split(' ');
+            List<string> reverseWordsArr = new List<string> ();
+
+            foreach (var word in wordsArr)
+            {
+                var reversedWord = "";
+                for (var i = word.Length - 1; i >= 0; i--)
+                {
+                    reversedWord += word[i];
+                }
+                reverseWordsArr.Add(reversedWord);
+            }
+
+            return string.Join(" ", reverseWordsArr);
+        }
+    }
+}
