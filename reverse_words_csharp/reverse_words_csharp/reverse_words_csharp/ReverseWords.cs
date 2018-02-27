1c4bc4366de612f1368aa8aca39d9c9b3d5bd5a3
Dennis J Daubney Jr <dennis.daubney@gmail.com>
2018-02-26 21:01:26 -0500

    added comments

--- reverse_words_csharp/reverse_words_csharp/reverse_words_csharp/ReverseWords.cs
+++ reverse_words_csharp/reverse_words_csharp/reverse_words_csharp/ReverseWords.cs
@@ -10,16 +10,21 @@ namespace reverse_words_csharp
     {
         public string reverseWords(string input)
         {
+            // split input words on space character
             string[] wordsArr = input.Split(' ');
+            // c# does not allow push to arrays, so create a list string array to store the reversed words
             List<string> reverseWordsArr = new List<string> ();
 
+            // iterate through input string and reverse words
             foreach (var word in wordsArr)
             {
+                // create empty variable to store reversed string
                 var reversedWord = "";
                 for (var i = word.Length - 1; i >= 0; i--)
                 {
                     reversedWord += word[i];
                 }
+                // use Add() method to add each reversed word to the list
                 reverseWordsArr.Add(reversedWord);
             }
 
